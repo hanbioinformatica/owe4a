e8b6b31d67229827b01729a291d060236ee1da8a
BioInformatica HAN University of Applied Sciences <hanbioinformatica@users.noreply.github.com>
2019-04-02 16:46:36 +0200

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello DEMO!"
+
+@app.route("/show")
+def hello():
+    return "<body bg="red">Hello DEMO!"
