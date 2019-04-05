c77d8b33f7e447f8f606accb50b0e02003170074
martijn <Martijn.vanderBruggen@han.nl>
2019-04-05 10:37:35 +0200

    piep toevoeging

--- application.py
+++ application.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 import mysql.connector
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return render_template('index.html')
+
 
 @app.route("/sql")
 def piep():
@@ -19,7 +21,7 @@ def piep():
     regel = ""
     tekst = ""
     while regel != None:
-        if len(regel)>9:
+        if len(regel) > 9:
             tekst += str(regel[9]) + "<br>"
         regel = cursor.fetchone()
 
@@ -29,4 +31,34 @@ def piep():
     return tekst
 
 
+@app.route("/piep")
+def piepapp():
+    hostname = "hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com"
+    woord = ""
+    tekst = ""
+    wachtwoord = request.args.get('ww')
+    query = "select voornaam, " \
+            " bericht, " \
+            " if(datum!=curdate(),datum,'vandaag ') datum," \
+            " if(datum!=curdate(),tijd, concat(convert(timediff(curtime(),tijd), char),' geleden')) tijd," \
+            " timestamp(datum,tijd) datumtijd" \
+            " from student " \
+            " natural join piep " \
+            " where bericht like '%{}%'" \
+            " order by datumtijd desc" \
+            " limit 100 ".format(woord)
 
+    conn = mysql.connector.connect(host=hostname,
+                                   user="dummy@" + hostname,
+                                   passwd=wachtwoord)
+    cursor = conn.cursor()
+    cursor.execute("use dummy")
+    cursor.execute(query)
+
+    for bericht in cursor:
+        tekst += bericht[0]+"<br>"
+        tekst += str(bericht[2]) + ' ' + bericht[3]+"<br>"
+        tekst += bericht[1]+"<br>"
+    cursor.close()
+    conn.close()
+    return tekst
