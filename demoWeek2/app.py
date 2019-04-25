129a5b05bfc17287ebd500dcf89ec211960e5aa2
martijn <Martijn.vanderBruggen@han.nl>
2019-04-25 17:21:36 +0200

    demo

---
+++ demoWeek2/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/demo',methods=['get','post'])
+def hello_world():
+    param_kleur = request.form.get("kleur")
+    if param_kleur==None: param_kleur="green"
+    return '''<body bgcolor="{}">Hello World! 
+    <form method="post">
+    <br>Kleur:<input type="text" name="kleur"><br>
+    Username: <input type="text" name="username"><br>
+    Wachtwoord: <input type="password" name="wachtwoord"><br>
+    <input type="submit" value="klik">
+    </form>
+    '''.format(param_kleur)
+
+
+@app.route('/')
+def hello_world2():
+    return "dit is de start"
+
+
+if __name__ == '__main__':
+    app.run()
