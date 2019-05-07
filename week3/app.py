16e3770f88889d3b58d8a41184b63fc1b39a1912
martijn <Martijn.vanderBruggen@han.nl>
2019-05-07 16:15:04 +0200

    azureReady

--- week3/app.py
+++ week3/app.py
@@ -1,25 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/demo',methods=['get','post'])
+@app.route('/',methods=['get','post'])
 def hello_world():
     param_kleur = request.form.get("kleur")
     if param_kleur==None: param_kleur="green"
-    return '''<head><link rel="stylesheet" href="https://unpkg.com/picnic"></head><body bgcolor="{}">Hello World! 
-    <form method="post">
-    <br>Kleur:<input type="text" name="kleur"><br>
-    Username: <input type="text" name="username"><br>
-    Wachtwoord: <input type="password" name="wachtwoord"><br>
-    <input type="submit" value="klik">
-    </form>
-    '''.format(param_kleur)
-
-
-@app.route('/')
-def hello_world2():
-    return "dit is de start"
+    resp = make_response(render_template('myTemplate.html',kleur = param_kleur))
+    return resp
 
 
 if __name__ == '__main__':
