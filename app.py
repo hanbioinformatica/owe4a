cb81f15a685aa8da6cbde6ee7f07fd28e7928838
martijn <Martijn.vanderBruggen@han.nl>
2019-05-07 17:26:21 +0200

    azureLiveDemo

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     param_kleur = request.form.get("kleur")
     if param_kleur==None: param_kleur="green"
-    return "<marquee>Hello World!</marquee>"
+    return "<marquee>Hello Bioinformatica!</marquee>"
 
 
 if __name__ == '__main__':
