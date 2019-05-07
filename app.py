8ac46605805cf3b28405bcc564dae2916e621c44
martijn <Martijn.vanderBruggen@han.nl>
2019-05-07 16:25:20 +0200

    azureRootApp

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, make_response, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['get','post'])
+def hello_world():
+    param_kleur = request.form.get("kleur")
+    if param_kleur==None: param_kleur="green"
+    resp = make_response(render_template('myTemplate.html',kleur = param_kleur))
+    return resp
+
+
+if __name__ == '__main__':
+    app.run()
