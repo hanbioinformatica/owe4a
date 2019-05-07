1b634e939ad546532c65cc0a3bedad17147b9ebf
martijn <Martijn.vanderBruggen@han.nl>
2019-05-07 16:27:00 +0200

    azureRootApp

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def hello_world():
     param_kleur = request.form.get("kleur")
     if param_kleur==None: param_kleur="green"
-    resp = make_response(render_template('myTemplate.html',kleur = param_kleur))
-    return resp
+    #resp = make_response(render_template('myTemplate.html',kleur = param_kleur))
+    return "Hello World!"
 
 
 if __name__ == '__main__':
