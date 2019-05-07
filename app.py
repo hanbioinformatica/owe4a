08d22c5a2ef1d76e9ef33abeba6e8243cab4a5fb
martijn <Martijn.vanderBruggen@han.nl>
2019-05-07 17:18:00 +0200

    azureRootApp

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 def hello_world():
     param_kleur = request.form.get("kleur")
     if param_kleur==None: param_kleur="green"
-    #resp = make_response(render_template('myTemplate.html',kleur = param_kleur))
-    return "Hello World!"
+    return "<marquee>Hello World!</marquee>"
 
 
 if __name__ == '__main__':
--- week3/app.py
+++ week3/app.py
@@ -3,11 +3,18 @@ from flask import Flask, request, make_response, render_template
 app = Flask(__name__)
 
 
-@app.route('/',methods=['get','post'])
+@app.route('/', methods=['get', 'post'])
 def hello_world():
     param_kleur = request.form.get("kleur")
-    if param_kleur==None: param_kleur="green"
-    resp = make_response(render_template('myTemplate.html',kleur = param_kleur))
+    if param_kleur == None: param_kleur = "green"
+    resp = """<body bgcolor="{}">Hello World!
+    <form method="post">
+    <br>Kleur:<input type="text" name="kleur"><br>
+    Username: <input type="text" name="username"><br>
+    Wachtwoord: <input type="password" name="wachtwoord"><br>
+    <input type="submit" value="klik">
+    </form>
+    </body>""".format(param_kleur)
     return resp
 
 
